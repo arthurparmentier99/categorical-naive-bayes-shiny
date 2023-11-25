b34c3e7894d51690cb44ab4edd3a3aac19132de8
arthurparmentier99 <146182092+arthurparmentier99@users.noreply.github.com>
2023-11-25 02:21:21 +0100

    classe R6 fonctionnelle avec quali et quanti

---
+++ R6_finish.R
@@ -0,0 +1,365 @@
+install.packages("R6", repos="https://cloud.r-project.org/")
+library(R6)
+library(parallel)
+
+NaiveBayes <- R6Class("NaiveBayes",
+                      public = list(
+                        data = NA,
+                        features = NA,
+                        target = NA,
+                        prior.probabilities = NA,
+                        conditional.probabilities = list(),
+                        X = NA,
+                        y = NA,
+                        classes = NA,
+                        groupes = NA,
+                        nb.is.numeric = FALSE,
+                        
+                        initialize = function(data, features, target) {
+                          # check if there is data
+                          if(missing(data)){
+                            stop("You requires a data file")
+                          }
+                          
+                          # check if we have at least one feature
+                          if (missing(features)) {
+                            stop("There must be at least one explanatory variable.")
+                          }
+                          
+                          # check if the target is qualitative
+                          if(!is.character(target) & !is.factor(target)){
+                            stop("The target variable is necessarily qualitative.")
+                          }
+                          
+                          self$data <- data
+                          self$features <- features
+                          self$target <- target
+                        },
+                        
+                        # the fit function is used to calculate the various probabilities
+                        fit = function(X, y, epsilon = 0.001) {
+                          # check if there is data
+                          if(missing(X)){
+                            stop("Fit method requires a dataframe")
+                          }
+                          
+                          # check if the target is qualitative
+                          if(!is.character(y) & !is.factor(y)){
+                            stop("The target variable is necessarily qualitative.")
+                          }
+                          
+                          # get the predictor variables (will be usefull for checking type of columns)
+                          self$X <- X
+                          
+                          # check if their is a numerical column in the dataframe
+                          self$check.numerical()
+                          
+                          # transform the target variable in factor if not
+                          if (!is.factor(y)) {
+                            y <- as.factor(y)
+                          }
+                          # get the target variable (will be useful for predict)
+                          self$y <- y
+                          # get the possible predictions classes (will be useful for predict)
+                          self$classes <- levels(y)
+                          # calculate prior probabilities
+                          self$prior.probabilities <- table(y) / nrow(X)
+                          
+                          
+                          ## calculate conditional probabilities
+                          #self$conditional.probabilities <- lapply(self$features, function(feature) {
+                          #  # apply the prop.table on each feature with the target to get their conditional probabilities
+                          #  feature.conditional.prob <- prop.table(table(self$X[[feature]], self$y) + epsilon, margin = 2)
+                          #  return(feature.conditional.prob)
+                          #})
+                          
+                          
+                          # detect the number of cores available for the parallelisation
+                          num_cores <- detectCores()
+                          cl <- makeCluster(num_cores)
+                          # calculate conditional probabilities
+                          self$conditional.probabilities <- parLapply(cl, self$features, function(feature) {
+                            # apply the prop.table on each feature with the target to get their conditional probabilities
+                            feature.conditional.prob <- prop.table(table(self$X[[feature]], self$y) + epsilon, margin = 2)
+                            return(feature.conditional.prob)
+                          })
+                          stopCluster(cl)
+                          
+                        },
+                        
+                        predict = function(X) {
+                          if (self$nb.is.numeric) {
+                            X <- self$transform.categorical(X)
+                          }
+                          
+                          # vector of the predicted classes
+                          predicted.classes <- c()
+                          
+                          # for each row of the dataframe
+                          for (i in 1:nrow(X)) {
+                            # create a vector of the size of all possible probabilities
+                            probabilities <- numeric(length(self$classes))
+                            # initialize the counter
+                            counter <- 1
+                            # for each possible class
+                            for (class in levels(self$y)) {
+                              # initialize the probability with the prior_probability of the class
+                              probabilities[counter] <- self$prior.probabilities[class]
+                              # for each predictor variable
+                              for (j in seq_along(X)) {
+                                # multiply with the probability of P(feature | class)
+                                probabilities[counter] <- probabilities[counter] * self$conditional.probabilities[[j]][X[[i, j]], class]
+                              }
+                              counter <- counter + 1
+                            }
+                            
+                            # normalize the probabilities
+                            probabilities <- probabilities / sum(probabilities)
+                            
+                            # get the predicted class with the max probabilities
+                            predict.classe <- self$classes[which.max(probabilities)]
+                            # add it to the vector
+                            predicted.classes <- c(predicted.classes, predict.classe)
+                          }
+                          # add a column prediction to X
+                          X$Prediction <- predicted.classes
+                          # return X with the predictions
+                          return(X)
+                        },
+                        
+                        predict_proba = function(X) {
+                          if (self$nb.is.numeric) {
+                            X <- self$transform.categorical(X)
+                          }
+                          
+                          # dataframe of class membership probabilities
+                          probabilities.classes <- data.frame(matrix(NA, nrow = 0, ncol = length(self$classes)))
+                          
+                          # for each row of the dataframe
+                          for (i in 1:nrow(X)) {
+                            # create a vector of the size of all possible probabilities
+                            probabilities <- numeric(length(self$classes))
+                            # initialize the counter
+                            counter <- 1
+                            # for each possible class
+                            for (class in levels(self$y)) {
+                              # initialize the probability with the prior_probability of the class
+                              probabilities[counter] <- self$prior.probabilities[class]
+                              # for each predictor variable
+                              for (j in seq_along(X)) {
+                                # multiply with the probability of P(feature | class)
+                                probabilities[counter] <- probabilities[counter] * self$conditional.probabilities[[j]][X[[i, j]], class]
+                              }
+                              counter <- counter + 1
+                            }
+
+                            # normalize the probabilities
+                            probabilities <- probabilities / sum(probabilities)
+                            # add the probabilities to the dataframe
+                            probabilities.classes <- rbind(probabilities.classes, probabilities)
+                          }
+                          # change the name of the columns with the classes names
+                          colnames(probabilities.classes) <- self$classes
+                          # return the dataframe of class membership probabilities
+                          return(probabilities.classes)
+                        },
+                        
+                        print = function() {
+                          print("TEST DU PRINT")
+                        },
+                        
+                        summary = function() {
+                          print("TEST DU SUMMARY")
+                        },
+                        
+                        # check if columns are numerical and transform them in categorical
+                        check.numerical = function() {
+                          # if its the first time (train in general)
+                          if (!self$nb.is.numeric) {
+                            # create a dataframe for the groupes with the number of explanatory variables
+                            # the number of row will always be 4 because we use quantiles and median
+                            self$groupes <- as.data.frame(matrix(ncol = 0, nrow = 4))
+                          }
+                          
+                          for (i in 1:ncol(self$X)) {
+                            if (is.numeric(self$X[, i])) {
+                              self$nb.is.numeric <- TRUE
+                              self$X[, i] <- self$numerical.to.categorical(self$X[, i], i) 
+                            }
+                          }
+                        },
+                        
+                        
+                        # too long because we can't work with big dataset with Shiny
+                        #check.numerical_parallel = function() {
+                        #  # detect the number of cores available for parallelization
+                        #  num_cores <- detectCores()
+                        #  cl <- makeCluster(num_cores)
+                        #  
+                        #  # parLapply to check numerical columns in parallel
+                        #  self$X <- parLapply(cl, self$X, function(column) {
+                        #    if (is.numeric(column)) {
+                        #      return(self$numerical.to.categorical(column))
+                        #    } else {
+                        #      return(column)
+                        #    }
+                        #  })
+                        #  
+                        #  stopCluster(cl)
+                        #},
+                        
+                        
+                        # transform a numeric column in a categorical column
+                        numerical.to.categorical = function(column, column_number) {
+                          
+                          # create the breaks for the quantiles and the median
+                          breaks <- quantile(column, probs = seq(0, 1, length.out = 5))
+                          # create the groupes on the columns with the breaks
+                          groupes <- cut(column, breaks = breaks, include.lowest = TRUE)
+                          
+                          # intermediate variable to stock the groupes in a list
+                          groupes_list <- vector("list", length = 4)
+                          # add the groupes to the groupes dataframe (useful for encoding test values)
+                          # for each levels of groupes (will always be 4 because we use quantiles and median)
+                          for (i in seq_along(levels(groupes))) {
+                            # extraction of levels for each column
+                            groupes_list[[i]] <- levels(groupes)[i]
+                          }
+                          
+                          # add the groupes to the dataframe in the right column name
+                          self$groupes[[names(self$X[column_number])]] <- groupes_list
+                          
+                          # renames the groupes with letter (categorical)
+                          levels(groupes)[1] <- "A"
+                          levels(groupes)[2] <- "B"
+                          levels(groupes)[3] <- "C"
+                          levels(groupes)[4] <- "D"
+                          
+                          # we return the column with the groupes
+                          return(groupes)
+                        },
+                        
+                        transform.categorical = function(X) {
+                          # for each column of our groupes
+                          for (i in 1:length(self$groupes)) {
+                            # we get the intervals of the groupes that we created
+                            intervals <- unlist(self$groupes[[i]])
+                            # for each row of the dataframe X
+                            for (k in 1:nrow(X)) {
+                              # for each interval
+                              for (j in 1:length(intervals)) {
+                                # cleaning the intervals
+                                intervals[j] <- str_replace(intervals[j], "\\[", "")
+                                intervals[j] <- str_replace(intervals[j], "\\]", "")
+                                intervals[j] <- str_replace(intervals[j], "\\(", "")
+                                intervals[j] <- str_replace(intervals[j], ",", " ")
+                                # get the cleaned interval in numeric
+                                numeric.interval <- as.numeric(unlist(strsplit(intervals[j], " ")))
+                                
+                                # check if the value in the dataframe is in the interval of our groups
+                                is.in.interval <- X[k ,i] >= numeric.interval[1] & X[k, i] < numeric.interval[2]
+                                
+                                # if he is in the interval
+                                if (is.in.interval) {
+                                  if (j == 1) {
+                                    # first interval will always be category A
+                                    X[k, i] <- "A"
+                                    break
+                                  } else if (j == 2) {
+                                    # second interval will always be category B
+                                    X[k, i] <- "B"
+                                    break
+                                  } else if (j == 3) {
+                                    # third interval will always be category C
+                                    X[k, i] <- "C"
+                                    break
+                                  } else {
+                                    # last interval will always be category D
+                                    X[k, i] <- "D"
+                                    break
+                                  }
+                                }
+                              }
+                            }
+                            
+                          }
+                          return(X)
+                        }
+                        
+                      )
+)
+
+# TEST FACILE MARCHE
+
+nb <- NaiveBayes$new(data, , "A")
+
+# train data
+data <- data.frame(
+  Feature1 = c("A", "B", "C","B","C","C"),
+  Feature2 = c("X", "X", "Z","Y","Z","Z"),
+  Class = c("Positive", "Negative","Positive","Positive","Negative","Positive")
+)
+
+X=data[, c("Feature1", "Feature2")]
+y=data[, "Class"]
+
+# test data
+new_data <- data.frame(
+  Feature1 = c("A", "B", "C"),
+  Feature2 = c("X", "Y", "Z")
+)
+
+nb <- NaiveBayes$new(data, c("Feature1", "Feature2"), "Class")
+nb$fit(X, y)
+nb$conditional.probabilities
+p <- nb$predict(X)
+p
+pb <- nb$predict_proba(X)
+pb
+
+
+# TEST AVEC IRIS (A REGLER)
+X <- iris[, c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")]
+y <- iris[, "Species"]
+X_test <- data.frame(
+  Sepal.Length = c(5.3, 4.4, 5.7, 4.6, 5.2, 4.9),
+  Sepal.Width = c(3.5, 3.4, 3.1, 3.1, 3.2, 3.0),
+  Petal.Length = c(1.6, 1.4, 1.3, 1.5, 1.4, 1.7),
+  Petal.Width = c(0.3, 0.2, 0.3, 0.2, 0.2, 0.4)
+)
+nb2 <- NaiveBayes$new(iris, c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"), "Species")
+nb2$fit(X, y)
+
+nb2$predict(X_test)
+nb2$predict_proba(X_test)
+nb2$conditional.probabilities
+nb2$prior.probabilities
+
+
+# TEST AVEC DONNEES COMPLETES
+healthcare_data <- read.csv("healthcare_dataset.csv")
+# Definir la seed pour la reproductibilite
+set.seed(123)
+
+library(caret)
+# Spécifier les colonnes X et y
+selected_columns <- c("Medical.Condition", "Admission.Type","Test.Results")
+
+# Diviser les donnees en ensembles d'entrainement (70%) et de test (30%)
+train_index <- createDataPartition(healthcare_data$Test.Results, p = 0.7, list = FALSE)
+train_data <- healthcare_data[train_index,selected_columns ]
+test_data <- healthcare_data[-train_index,selected_columns ]
+
+X_train <- train_data[, -3]  # Remplacez colIndex par l'indice de la colonne cible
+y_train <- train_data[, 3]   # Remplacez colIndex par l'indice de la colonne cible
+X_test <- test_data[, -3]    # Remplacez colIndex par l'indice de la colonne cible
+y_test <- test_data[, 3]     # Remplacez colIndex par l'indice de la colonne cible
+
+nbh <- NaiveBayes$new(healthcare_data, c("Medical.Condition", "Admission.Type"), "Test.Results")
+nbh$fit(X_train, y_train)
+nbh$prior.probabilities
+nbh$conditional.probabilities
+nbh$predict(X_test)
+nbh$predict_proba(X_test)
+
+
